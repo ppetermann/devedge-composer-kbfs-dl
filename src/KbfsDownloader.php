ffcd9fd8520e37c94e3cfe6b8f75fb1aea56b6c8
Peter Petermann <peter.petermann@gameforge.com>
2016-07-11 19:14:28 +0200

    extend path downloader for now

--- src/KbfsDownloader.php
+++ src/KbfsDownloader.php
@@ -2,9 +2,11 @@
 namespace Devedge\KbfsDl;
 
 use Composer\Downloader\DownloaderInterface;
+use Composer\Downloader\FileDownloader;
+use Composer\Downloader\PathDownloader;
 use Composer\Package\PackageInterface;
 
-class KbfsDownloader implements DownloaderInterface
+class KbfsDownloader extends PathDownloader implements DownloaderInterface
 {
 
 }
