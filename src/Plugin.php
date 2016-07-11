d50e1903cc086fc29162781558b6ee122d42d3df
Peter Petermann <peter.petermann@gameforge.com>
2016-07-11 19:17:37 +0200

    initialize the downloader

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,8 @@ namespace Devedge\KbfsDl;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Util\Filesystem;
+use Composer\Util\ProcessExecutor;
 
 class Plugin implements PluginInterface
 {
@@ -16,6 +18,13 @@ class Plugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $composer->getDownloadManager()->setDownloader("kbfs");
+        // this is a bit cheesy, but seems to be the easiest way
+        $executor = new ProcessExecutor($io);
+        $fs = new Filesystem($executor);
+
+        $composer->getDownloadManager()->setDownloader(
+            "kbfs",
+            new KbfsDownloader($io, $composer->getConfig(), $executor, $fs)
+        );
     }
 }
